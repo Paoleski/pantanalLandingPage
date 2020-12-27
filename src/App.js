c42e6fd753266a91c0c63008aff1ec690c69f118
Paoleski <lucas.paoleschi@gmail.com>
2020-12-26 22:50:45 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import Section1 from './components/Section1';
 import Section2 from './components/Section2';
 import Section3 from './components/Section3';
